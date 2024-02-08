0d46132bc5ef7ffa865b8de2a3538b86ff0210f6
Brandon Rosero <brandonrosero34@gmail.com>
2024-02-08 16:32:04 -0600

    add screens

---
+++ screens/Forum.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+
+const Forum = () => {
+    return(
+
+        <View>
+            <Text>Forum</Text>
+        </View>
+
+    )
+}
+
+export default Forum
---
+++ screens/viewDonors.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+
+const ViewDonors = () => {
+    return(
+
+        <View>
+            <Text>View Donors</Text>
+        </View>
+
+    )
+}
+
+export default ViewDonors
