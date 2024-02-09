8e4ab7f01ddab5a4cfafff704a8168b5a7e5366c
Brandon Rosero <brandonrosero34@gmail.com>
2024-02-09 10:26:39 -0600

    add screens

--- screens/Forum.js
+++ screens/Forum.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 
 const Forum = () => {
+     
     return(
-
+        
         <View>
-            <Text>Forum</Text>
+            <Text>hi</Text>
         </View>
 
     )
