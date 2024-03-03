46268a072a790e68895fd6e108bb2ac135a1adeb
Brandon Rosero <104854684+brandon-rosero@users.noreply.github.com>
2024-03-03 17:02:57 -0600

    update misc

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import FoodBankHome from './components/FoodBankHome';
 import NavigationFoodBank from './components/NavigationFoodBank';
+import NavigationDonor from './components/NavigationDonor'
 
 export default function App() {
   
   return ( 
     <NavigationFoodBank />
+    //<NavigationDonor />
 
   );
 }
